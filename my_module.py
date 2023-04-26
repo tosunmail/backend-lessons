5e49366a25f2b241d8bf8b4948db3617314afe38
tosunmail <tosunmaildir@gmail.com>
2023-04-26 21:36:55 +0200

    modules

---
+++ my_module.py
@@ -0,0 +1,13 @@
+def plus(num1, num2):
+
+    '''
+    This function adds two integers
+    '''
+    return num1 + num2 
+
+
+def multiply(num1,num2):
+    '''
+    This function multiplies two integers
+    '''
+    return num1 * num2
